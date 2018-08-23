a414f4d4d0a7a129ee218f44fff3ac7e33200452
Mathias Lohne <mathialo@ifi.uio.no>
2018-08-23 14:06:03 +0200

    added some lines to style guide

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 # Install python package
-setup(name="tfwavelets",
-      version=0.1,
-      author="Kristian Monsen Haug, Mathias Lohne",
-      author_email="mathialo@ifi.uio.no",
-      license="MIT",
-      description="TensorFlow implementation of descrete wavelets",
-      url="https://github.com/UiO-CS/tf-wavelets",
-      install_requires=["tensorflow"],
-      packages=["tfwavelets"],
-      zip_safe=False)
+setup(
+    name="tfwavelets",
+    version=0.1,
+    author="Kristian Monsen Haug, Mathias Lohne",
+    author_email="mathialo@ifi.uio.no",
+    license="MIT",
+    description="TensorFlow implementation of descrete wavelets",
+    url="https://github.com/UiO-CS/tf-wavelets",
+    install_requires=["tensorflow"],
+    packages=["tfwavelets"],
+    zip_safe=False)
--- styleguide.md
+++ styleguide.md
@@ -6,5 +6,6 @@
  * Use 4 spaces for indentation
  * Use Google-style docstrings
  * Follow PEP-8 for naming of functions, classes, variables, etc
-
+ * Use pylint for error detection
+ * Use yapf for code formatting
 
