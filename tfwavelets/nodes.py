63aeb77542e3a11c1068922c6f3556eac53443d7
Mathias Lohne <mathialo@ifi.uio.no>
2018-08-29 13:55:55 +0200

    add TF nodes for 2D DWT, and update wrappers to use this node instead of two dwt1d

--- tfwavelets/nodes.py
+++ tfwavelets/nodes.py
@@ -34,3 +34,39 @@ def dwt1d(input_node, filter_coeffs, levels=1):
     coeffs[0] = last_level
     return tf.concat(coeffs, axis=1)
 
+
+def dwt2d(input_node, filter_coeffs, levels=1):
+    # TODO: Check that level is a reasonable number
+    # TODO: Check types
+
+    coeffs = [None] * levels
+
+    last_level = input_node
+    m, n = int(input_node.shape[0]), int(input_node.shape[1])
+
+    for level in range(levels):
+        local_m, local_n = m // (2 ** level), n // (2 ** level)
+
+        first_pass = dwt1d(last_level, filter_coeffs, 1)
+        second_pass = tf.transpose(
+            dwt1d(
+                tf.transpose(first_pass, perm=[1, 0, 2]),
+                filter_coeffs,
+                1
+            ),
+            perm=[1, 0, 2])
+
+        last_level = tf.slice(second_pass, [0, 0, 0], [local_m // 2, local_n // 2, 1])
+        coeffs[level] = [
+            tf.slice(second_pass, [0, local_n // 2, 0], [local_m // 2, local_n // 2, 1]),
+            tf.slice(second_pass, [local_m // 2, 0, 0], [local_m // 2, local_n // 2, 1]),
+            tf.slice(second_pass, [local_m // 2, local_n // 2, 0], [local_m // 2, local_n // 2, 1])
+        ]
+
+    for level in range(levels - 1, -1, -1):
+        upper_half = tf.concat([last_level, coeffs[level][0]], 0)
+        lower_half = tf.concat([coeffs[level][1], coeffs[level][2]], 0)
+
+        last_level = tf.concat([upper_half, lower_half], 1)
+
+    return last_level
--- tfwavelets/wrappers.py
+++ tfwavelets/wrappers.py
@@ -62,23 +62,17 @@ def dwt2d(signal, wavelet="haar", levels=1):
     signal = np.expand_dims(signal, -1)
 
     # Placeholder for input signal
-    tf_signal = tf.placeholder(dtype=tf.float32, shape=[None, None, None])
+    tf_signal = tf.placeholder(dtype=tf.float32, shape=signal.shape)
 
     # Get filter coeffs from database
     filter_coeffs = _parse_wavelet(wavelet)
 
-    # Set up tf graph, use dwt1d in two dimensions to achieve 2d dwt as this
-    # requires fewer floating point operations than a true 2d DWT
-    output = tfw.nodes.dwt1d(tf_signal, filter_coeffs, levels)
+    # Set up tf graph
+    output = tfw.nodes.dwt2d(tf_signal, filter_coeffs, levels)
 
-    # Compute graph two times, one for each dimension
+    # Compute graph
     with tf.Session() as sess:
-        # Apply 1D DWT to all rows
         signal = sess.run(output, feed_dict={tf_signal: signal})
-
-        # Apply 1D DWT to all cols
-        signal = np.swapaxes(
-            sess.run(output, feed_dict={tf_signal: np.swapaxes(signal, 0, 1)}), 0, 1)
 
     # Remove empty dimensions and return
     return np.squeeze(signal)
