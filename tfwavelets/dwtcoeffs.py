0f9b62707e12eaf5cebfea0771b55127266e4e27
Kristian Monsen Haug <kristian.haug95@gmail.com>
2018-08-29 08:50:59 +0200

    Implement function for calculating corners of filter matrices

--- tfwavelets/dwtcoeffs.py
+++ tfwavelets/dwtcoeffs.py
@@ -43,3 +43,44 @@ db4 = (np.array([-0.010597401784997278,
                  -0.032883011666982945,
                  -0.010597401784997278], dtype=np.float32))
 
+
+def edge_matrices(coeffs, zero):
+    """Computes the submatrices needed at the ends for circular convolution.
+
+    Args:
+        coeffs: 1d numpy array with the filter coefficients, ordered by
+                filter index.
+        zero:   Index in `coeffs` where the zero-indexed filter coefficient is.
+
+    Returns:
+        Tuple of 2d-arrays, (top-left, top-right, bottom-left, bottom-right).
+    """
+    if not isinstance(coeffs, np.ndarray):
+        coeffs = np.array(coeffs)
+
+    coeffs = coeffs[::-1]
+
+
+    n, = coeffs.shape
+    padding = max((zero, n-zero-1))
+    matrix_size = n+padding
+    filter_matrix = np.zeros((matrix_size, matrix_size), dtype=np.float32)
+    negative = coeffs[-(zero+1):] # negative indexed filter coeffs (and 0)
+    positive = coeffs[:-(zero+1)] # filter coeffs with strictly positive indeces
+
+    filter_matrix[0, :len(negative)] = negative
+    filter_matrix[0, -len(positive):] = positive
+
+    for i in range(1, matrix_size):
+        filter_matrix[i,:] = np.roll(filter_matrix[i-1,:], 1)
+
+
+    # TODO: Indexing not thoroughly tested
+    num_pos = len(positive)
+    num_neg = len(negative)
+    top_left = filter_matrix[:num_pos, :(num_pos+num_neg-1)]
+    top_right = filter_matrix[:num_pos, -num_pos:]
+    bottom_left = filter_matrix[-num_neg+1:, :num_neg-1]
+    bottom_right = filter_matrix[-num_neg+1:, -(num_pos+num_neg-1):]
+
+    return(top_left, top_right, bottom_left, bottom_right)
