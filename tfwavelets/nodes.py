365d52af8ee6f0ac1402f37023a32da2ae5c97da
Mathias Lohne <mathialo@ifi.uio.no>
2018-08-29 12:49:50 +0200

    extract filter adaption into a separate, reusable function

--- tfwavelets/nodes.py
+++ tfwavelets/nodes.py
@@ -1,22 +1,25 @@
 import tensorflow as tf
 import numpy as np
 
+
+def _adapt_filter(filter):
+    # Add empty dimensions for batch size and channel num
+    return np.expand_dims(np.expand_dims(filter, -1), -1)
+
+
 def dwt1d(input_node, filter_coeffs, levels=1):
     # TODO: Check that level is a reasonable number
     # TODO: Check types
 
-    coeffs = [None]*(levels+1)
+    coeffs = [None] * (levels + 1)
 
     last_level = input_node
-    
-    lp_adapted = np.expand_dims(filter_coeffs[0], -1)
-    lp_adapted = np.expand_dims(lp_adapted, -1)
 
-    hp_adapted = np.expand_dims(filter_coeffs[1], -1)
-    hp_adapted = np.expand_dims(hp_adapted, -1)
-    
-    tf_lp = tf.constant(lp_adapted, dtype=tf.float32, shape=[len(filter_coeffs[0]),1,1])
-    tf_hp = tf.constant(hp_adapted, dtype=tf.float32, shape=[len(filter_coeffs[1]),1,1])
+    lp_adapted = _adapt_filter(filter_coeffs[0])
+    hp_adapted = _adapt_filter(filter_coeffs[1])
+
+    tf_lp = tf.constant(lp_adapted, dtype=tf.float32, shape=[len(filter_coeffs[0]), 1, 1])
+    tf_hp = tf.constant(hp_adapted, dtype=tf.float32, shape=[len(filter_coeffs[1]), 1, 1])
 
     for level in range(levels):
         # TODO: Convert stride kwarg to tuple
@@ -26,8 +29,8 @@ def dwt1d(input_node, filter_coeffs, levels=1):
         hp_res = tf.nn.conv1d(last_level, tf_hp, stride=2, padding="SAME")
 
         last_level = lp_res
-        coeffs[levels-level] = hp_res
-
+        coeffs[levels - level] = hp_res
 
     coeffs[0] = last_level
     return tf.concat(coeffs, axis=1)
+
