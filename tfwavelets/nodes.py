767040632c9b9a3f9b9eea174afda912cc123980
Mathias Lohne <mathialo@ifi.uio.no>
2018-09-06 11:04:51 +0200

    Add documentation for dwt1d, dwt2d, _adapt_filter and _to_tf_mat

--- tfwavelets/nodes.py
+++ tfwavelets/nodes.py
@@ -4,11 +4,32 @@ from tfwavelets.dwtcoeffs import edge_matrices
 
 
 def _adapt_filter(filter):
+    """
+    Expands dimensions of a 1d vector to match the required tensor dimensions in a TF
+    graph.
+
+    Args:
+        filter (np.ndarray):     A 1D vector containing filter coefficients
+
+    Returns:
+        np.ndarray: A 3D vector with two empty dimensions as dim 2 and 3.
+
+    """
     # Add empty dimensions for batch size and channel num
     return np.expand_dims(np.expand_dims(filter, -1), -1)
 
 
 def _to_tf_mat(matrices):
+    """
+    Expands dimensions of 2D matrices to match the required tensor dimensions in a TF
+    graph, and wrapping them as TF constants.
+
+    Args:
+        matrices (iterable):    A list (or tuple) of 2D numpy arrays.
+
+    Returns:
+        iterable: A list of all the matrices converted to 3D TF tensors.
+    """
     result = []
 
     for matrix in matrices:
@@ -24,7 +45,7 @@ def cyclic_conv1d(input_node, kernel_node, tl_node, tr_node, bl_node, br_node):
 
     Args:
         input_node:  Input signal (3-tensor [batch, width, in_channels])
-        filter_node: Filter (3-tensor[filter_width, in_channels, out_channels]) 
+        kernel_node: Filter (3-tensor[filter_width, in_channels, out_channels])
         tl_node:     Top-left part of cyclic matrix
         tr_node:     Top-right part of cyclic matrix
         bl_node:     Bottom-left part of cyclic matrix
@@ -46,29 +67,41 @@ def cyclic_conv1d(input_node, kernel_node, tl_node, tr_node, bl_node, br_node):
                         [0, 0, 0],
                         [-1, tl_shape[2], -1])
     tr_slice = tf.slice(input_node,
-                        [0, input_shape[1]-tr_shape[2], 0],
-                        [-1, tr_shape[2], -1]) 
+                        [0, input_shape[1] - tr_shape[2], 0],
+                        [-1, tr_shape[2], -1])
     bl_slice = tf.slice(input_node,
                         [0, 0, 0],
                         [-1, bl_shape[2], -1])
     br_slice = tf.slice(input_node,
-                        [0, input_shape[1]-br_shape[2], 0],
+                        [0, input_shape[1] - br_shape[2], 0],
                         [-1, br_shape[2], -1])
 
-
     # TODO: It just werks (It's the magic of the algorithm). i.e. Why do we have to transpose?
-    tl = tl_node @ tf.transpose(tl_slice, perm=[2,1,0])
-    tr = tr_node @ tf.transpose(tr_slice, perm=[2,1,0])
-    bl = bl_node @ tf.transpose(bl_slice, perm=[2,1,0])
-    br = br_node @ tf.transpose(br_slice, perm=[2,1,0])
+    tl = tl_node @ tf.transpose(tl_slice, perm=[2, 1, 0])
+    tr = tr_node @ tf.transpose(tr_slice, perm=[2, 1, 0])
+    bl = bl_node @ tf.transpose(bl_slice, perm=[2, 1, 0])
+    br = br_node @ tf.transpose(br_slice, perm=[2, 1, 0])
 
-    head = tf.transpose(tl + tr, perm=[2,1,0])
-    tail = tf.transpose(bl + br, perm=[2,1,0])
+    head = tf.transpose(tl + tr, perm=[2, 1, 0])
+    tail = tf.transpose(bl + br, perm=[2, 1, 0])
 
     return tf.concat((head, inner, tail), axis=1)
 
 
 def dwt1d(input_node, filter_coeffs, levels=1):
+    """
+    Constructs a TF computational graph computing the 1D DWT of an input signal.
+
+    Args:
+        input_node:     A 3D tensor containing the signal. The dimensions should be
+                        [batch, signal, channels].
+        filter_coeffs:  A tuple of two 1D numpy arrays containing wavelet filter
+                        coefficients. First filter is LP, second is HP.
+        levels:         Number of levels.
+
+    Returns:
+        The output node of the DWT graph.
+    """
     # TODO: Check that level is a reasonable number
     # TODO: Check types
 
@@ -78,7 +111,6 @@ def dwt1d(input_node, filter_coeffs, levels=1):
 
     lp_adapted = _adapt_filter(filter_coeffs[0])
     lp_mat = _to_tf_mat(edge_matrices(filter_coeffs[0], 0))
-
 
     hp_adapted = _adapt_filter(filter_coeffs[1])
     hp_mat = _to_tf_mat(edge_matrices(filter_coeffs[1], 1))
@@ -93,8 +125,8 @@ def dwt1d(input_node, filter_coeffs, levels=1):
         # lp_res = tf.nn.conv1d(last_level, tf_lp, stride=2, padding="SAME")
         # hp_res = tf.nn.conv1d(last_level, tf_hp, stride=2, padding="SAME")
 
-        lp_res = cyclic_conv1d(last_level, tf_lp, *lp_mat)[:,::2,:]
-        hp_res = cyclic_conv1d(last_level, tf_hp, *hp_mat)[:,::2,:]
+        lp_res = cyclic_conv1d(last_level, tf_lp, *lp_mat)[:, ::2, :]
+        hp_res = cyclic_conv1d(last_level, tf_hp, *hp_mat)[:, ::2, :]
 
         last_level = lp_res
         coeffs[levels - level] = hp_res
@@ -104,6 +136,19 @@ def dwt1d(input_node, filter_coeffs, levels=1):
 
 
 def dwt2d(input_node, filter_coeffs, levels=1):
+    """
+    Constructs a TF computational graph computing the 2D DWT of an input signal.
+
+    Args:
+        input_node:     A 3D tensor containing the signal. The dimensions should be
+                        [rows, cols, channels].
+        filter_coeffs:  A tuple of two 1D numpy arrays containing wavelet filter
+                        coefficients. First filter is LP, second is HP.
+        levels:         Number of levels.
+
+    Returns:
+        The output node of the DWT graph.
+    """
     # TODO: Check that level is a reasonable number
     # TODO: Check types
 
