9044f8d27bb2a66139e62a7ef65ba7048b359e71
Mathias Lohne <mathialo@ifi.uio.no>
2018-09-11 14:20:03 +0200

    Restructure wrappers.py to reuse code more efficiently. Add wrappers to IDWTs

--- tfwavelets/wrappers.py
+++ tfwavelets/wrappers.py
@@ -30,21 +30,13 @@ def dwt1d(signal, wavelet="haar", levels=1):
     signal = np.expand_dims(signal, 0)
     signal = np.expand_dims(signal, -1)
 
-    # Placeholder for input signal
-    tf_signal = tf.placeholder(dtype=tf.float32, shape=[None, None, None])
-
-    # Get filter coeffs from database
-    filter_coeffs = _parse_wavelet(wavelet)
-
-    # Set up tf graph
-    output = tfw.nodes.dwt1d(tf_signal, filter_coeffs, levels)
-
-    # Compute graph
-    with tf.Session() as sess:
-        signal = sess.run(output, feed_dict={tf_signal: signal})
-
-    # Remove empty dimensions and return
-    return np.squeeze(signal)
+    # Construct and compute TF graph
+    return _construct_and_compute_graph(
+        signal,
+        tfw.nodes.dwt1d,
+        _parse_wavelet(wavelet),
+        levels
+    )
 
 
 def dwt2d(signal, wavelet="haar", levels=1):
@@ -67,18 +59,96 @@ def dwt2d(signal, wavelet="haar", levels=1):
     signal = signal.astype(np.float32)
     signal = np.expand_dims(signal, -1)
 
-    # Placeholder for input signal
-    tf_signal = tf.placeholder(dtype=tf.float32, shape=signal.shape)
+    # Construct and compute TF graph
+    return _construct_and_compute_graph(
+        signal,
+        tfw.nodes.dwt2d,
+        _parse_wavelet(wavelet),
+        levels
+    )
 
-    # Get filter coeffs from database
-    filter_coeffs = _parse_wavelet(wavelet)
+
+def idwt1d(signal, wavelet="haar", levels=1):
+    """
+    Computes the inverse DWT of a 1D signal.
+
+    Args:
+        signal (np.ndarray):    A 1D array to compute IDWT of.
+        wavelet (str):          Type of wavelet (haar or db2)
+        levels (int):           Number of DWT levels
+
+    Returns:
+        np.ndarray: The IDWT of the signal.
+
+    Raises:
+        ValueError: If wavelet is not supported
+    """
+    # Prepare signal for tf. Turn into 32bit floats for GPU computation, and
+    # expand dims to make it into a 3d tensor so tf.nn.conv1d is happy
+    signal = signal.astype(np.float32)
+    signal = np.expand_dims(signal, 0)
+    signal = np.expand_dims(signal, -1)
+
+    # Construct and compute TF graph
+    return _construct_and_compute_graph(
+        signal,
+        tfw.nodes.idwt1d,
+        _parse_wavelet(wavelet),
+        levels
+    )
+
+
+def idwt2d(signal, wavelet="haar", levels=1):
+    """
+    Computes the inverse DWT of a 2D signal.
+
+    Args:
+        signal (np.ndarray):    A 2D array to compute iDWT of.
+        wavelet (str):          Type of wavelet (haar or db2)
+        levels (int):           Number of DWT levels
+
+    Returns:
+        np.ndarray: The IDWT of the signal.
+
+    Raises:
+        ValueError: If wavelet is not supported
+    """
+    # Prepare signal for tf. Turn into 32bit floats for GPU computation, and
+    # expand dims to make it into a 3d tensor so tf.nn.conv1d is happy
+    signal = signal.astype(np.float32)
+    signal = np.expand_dims(signal, -1)
+
+    # Construct and compute TF graph
+    return _construct_and_compute_graph(
+        signal,
+        tfw.nodes.idwt2d,
+        _parse_wavelet(wavelet),
+        levels
+    )
+
+
+def _construct_and_compute_graph(input_signal, node, wavelet_obj, levels):
+    """
+    Constructs a TF graph processing the input signal with given node and evaluates it.
+
+    Args:
+        input_signal:       Input signal. A 3D array with [batch, signal, channels]
+        node:               Node to process signal with, any kind of dwt/idwt
+        wavelet_obj:        Wavelet object to pass to node
+        levels:             Num of levels (passed to node)
+
+    Returns:
+
+    """
+    # Placeholder for input signal
+    tf_signal = tf.placeholder(dtype=tf.float32, shape=input_signal.shape)
 
     # Set up tf graph
-    output = tfw.nodes.dwt2d(tf_signal, filter_coeffs, levels)
+    output = node(tf_signal, wavelet=wavelet_obj, levels=levels)
 
     # Compute graph
     with tf.Session() as sess:
-        signal = sess.run(output, feed_dict={tf_signal: signal})
+        signal = sess.run(output, feed_dict={tf_signal: input_signal})
 
     # Remove empty dimensions and return
     return np.squeeze(signal)
