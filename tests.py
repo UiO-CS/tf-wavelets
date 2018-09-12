33e25d1126b7b7938022b00ea9035e7d320fed95
Mathias Lohne <mathialo@ifi.uio.no>
2018-09-12 11:32:10 +0200

    Add tests for inverse

--- tests.py
+++ tests.py
@@ -14,7 +14,9 @@ def check_orthonormality_1d(wavelet, tol=1e-5, N=8):
     error1 = np.sum(np.abs(matrix.T @ matrix - np.eye(N))) / N
     error2 = np.sum(np.abs(matrix @ matrix.T - np.eye(N))) / N
     assert error1 < tol, "Mean error: %g" % error1
+    print("Mean error: %g" % error1)
     assert error2 < tol, "Mean error: %g" % error2
+    print("Mean error: %g" % error2)
 
 
 def check_linearity_1d(wavelet, tol=1e-5, N=256):
@@ -29,6 +31,7 @@ def check_linearity_1d(wavelet, tol=1e-5, N=256):
 
     error = np.sum(np.abs(test1 - test2)) / (N ** 2)
     assert error < tol, "Mean error: %g" % error
+    print("Mean error: %g" % error)
 
 
 def check_linearity_2d(wavelet, tol=1e-5, N=256):
@@ -43,6 +46,33 @@ def check_linearity_2d(wavelet, tol=1e-5, N=256):
 
     error = np.sum(np.abs(test1 - test2)) / (N ** 2)
     assert error < tol, "Mean error: %g" % error
+    print("Mean error: %g" % error)
+
+
+def check_inverse_1d(wavelet, levels=1, tol=1e-4, N=256):
+    signal = np.random.random(N)
+
+    reconstructed = tfw.wrappers.idwt1d(
+        tfw.wrappers.dwt1d(signal, levels=levels),
+        levels=levels
+    )
+
+    error = np.sum(np.abs(signal - reconstructed)) / (N)
+    assert error < tol, "Mean error: %g" % error
+    print("Mean error: %g" % error)
+
+
+def check_inverse_2d(wavelet, levels=1, tol=1e-4, N=256):
+    signal = np.random.random((N, N))
+
+    reconstructed = tfw.wrappers.idwt2d(
+        tfw.wrappers.dwt2d(signal, levels=levels),
+        levels=levels
+    )
+
+    error = np.sum(np.abs(signal - reconstructed)) / (N)
+    assert error < tol, "Mean error: %g" % error
+    print("Mean error: %g" % error)
 
 
 def test_ortho_haar():
@@ -69,10 +99,30 @@ def test_linear_db2_2d():
     check_linearity_2d("db2")
 
 
+def test_inverse_haar_1d():
+    check_inverse_1d("haar", levels=1)
+
+
+def test_inverse_db2_1d():
+    check_inverse_1d("db2", levels=1)
+
+
+def test_inverse_haar_2d():
+    check_inverse_2d("haar", levels=2)
+
+
+def test_inverse_db2_2d():
+    check_inverse_2d("db2", levels=2)
+
+
 if __name__ == "__main__":
-    test_linear_db2_1d()
     test_linear_haar_1d()
+    test_linear_db2_1d()
     test_linear_db2_2d()
     test_linear_haar_2d()
     test_ortho_db2()
     test_ortho_haar()
+    test_inverse_db2_2d()
+    test_inverse_haar_2d()
+    test_inverse_db2_1d()
+    test_inverse_haar_1d()
